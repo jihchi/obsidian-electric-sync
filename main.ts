a296147f2ef7cd8d61fc5da1c8d9c26263a60b58
Jihchi Lee <achi@987.tw>
2024-12-17 19:54:49 +0200

    Rename `MyPlugin` to `ObsidianElectricSync`

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface ObsidianElectricSyncSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: ObsidianElectricSyncSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ObsidianElectricSync extends Plugin {
+	settings: ObsidianElectricSyncSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: ObsidianElectricSync;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ObsidianElectricSync) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
